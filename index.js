e4d9ceeae5321a2527f55d08b3f74e7980640dc4
Juan <jd.rodriguez.or@gmail.com>
2023-04-12 20:09:17 -0400

    Update styles.css

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-
-// function displayName(){
-//     warriorClass = document.getElementById("myInput").value;
-//     console.log(typeof(warriorClass))
-//     document.getElementById("myClass").textContent = warriorClass;
-//     console.log(warriorClass);
-
-// }
-
-// document.getElementById("myButton").addEventListener("click", displayName);
-
-//Create a small program to calculate the hypo of a triangle
-
-/*
-
-Divide the problem in small sections
-
-1- Collect user input a & b legs
-2- Manipulate date types
-3- Compute equation
-4- Display values
-*/
-
-// let a, b, hypo;
-
-// a = Number(window.prompt("Enter the dimension of a: "));
-// b = Number(window.prompt("Enter the dimension of b; "));
-
-// hypo = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-// console.log("Side C: ", hypo);
-// alert(hypo);
-
-// 1- Create the html to let the user enter the information
-// 2- Create functions to collect information from html elements
 let a, b, c;
 
 
